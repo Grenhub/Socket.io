69d187cee2a2861d8970a216de213847ff7a9b1d
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-09-08 10:26:06 +0200

    rooms

--- index.js
+++ index.js
@@ -11,23 +11,41 @@ const {
   removeUserFromList,
 } = require("./modules/users");
 
+const { checkPassword } = require('./modules/rooms');
+
 //Serves a static file from public folder
 app.use(express.static("public"));
 
 //All connecting users
 io.on("connection", (socket) => {
-  /* console.log('A user has connected..'); */
-
   //Saves current user to userlist
-  socket.on("joinChat", (username) => {
-    const user = addUserToList(username, socket.id);
-    /* console.log(user); */
+  socket.on("joinChat", (incoming) => {
+    //Add user to user list
+    const user = addUserToList(incoming.user, socket.id, incoming.room);
+
+    //Check password
+    let correctPwd = checkPassword(incoming.room, incoming.pwd);
+  
+    //If password is wrong send back false
+    if(!correctPwd) {
+      socket.emit('joined', { pwd: false }); 
+    } else {
+      socket.emit('joined', { pwd: true, room: incoming.room });
+    }
+
+    //User joins room
+    socket.join(incoming.room);
+
+    //Send message to the user that is connecting
+    socket.emit('message', { message: `Welcome to chatroom ${incoming.room}`, userName: 'Bot' });
+
   });
 
   //Listen for messages from client
   socket.on("message", (msg) => {
-    /* console.log("Message: " + msg.message + " Username: " + msg.userName); */
-    io.emit("message", msg);
+    const user = getUserFromList(socket.id);
+    //Send message to specific room, to everyone except user
+    io.to(user.room).emit("message", msg);
   });
 
   //Listen for GIF
@@ -39,19 +57,18 @@ io.on("connection", (socket) => {
 
   //Runs when someone is typing
   socket.on("typing", (incoming) => {
-    /*  console.log(
-      `it is ${incoming.typing} that ${incoming.userName} is typing: `
-    ); */
+    const user = getUserFromList(socket.id);
+    
     //Sends to everyone except current user
-    socket.broadcast.emit("typing", incoming);
+    socket.broadcast.to(user.room).emit("typing", incoming);
   });
 
   //Runs when a user disconnects
   socket.on("disconnect", () => {
     const user = removeUserFromList(socket.id);
     if (user) {
-      console.log(`${user.username} disconnected`);
-      io.emit("leaving", user.username);
+      //Send to the specific room that user left
+      io.to(user.room).emit("leaving", user.username);
     }
   });
 });
